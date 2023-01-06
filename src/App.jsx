9ad2cc833fe98e53b24732f6c378690bc0d017fd
Mohini Mahato <mohinimahatowork@gmail.com>
2023-01-07 00:25:57 +0530

    updates+fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+// import React from "react";
+import { createRoot } from "react-dom/client";
+import Header from "./components/Header";
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, " Notes-App!")
+  return (
+    <div>
+      <h1>My Notes</h1>
+      <Header name="New Notes" />
+      <input />
+    </div>
   );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const root = createRoot(container);
+root.render(<App />);
